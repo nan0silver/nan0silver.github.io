0fe1455e4be9b4b39ac0af4ff1240c079e62e4db
nan0silver <nan0silver@users.noreply.github.com>
2025-03-18 07:42:37 +0000

    deploy: 85f29f77764cd307a5bc6f6de564ee1276dc479b

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -219,20 +219,20 @@ var tipuesearch = {"pages": [{
     "tags": "algorithm",
     "url": "/algorithm/2024-07-08-day1/"
   },{
-    "title": "[Flutter] URI과 URL의 차이",
+    "title": "[Etc] URI과 URL의 차이",
     "text": "목차 URI URL URN URI와 URL의 차이점 flutter앱을 개발하는 도중, http통신관련 코드를 짜면서 Uri.parse()함수를 여러 번 접하게 되었다. 주소 관련 변수명을 url로 쓰고 있어 이 둘의 의미가 혼동되어 정확하게 알아보고자 하였다. URI (Uniform Resource Identifier) 우리 말로 ‘통합 자원 식별자’ Uniform -&gt; 리소스를 식별는 통일된 방식 Resource -&gt; URI로 식별이 가능한 웹 브라우저 파일 및 그 이외의 리소스를 포함하는 모든 종류의 자원 Identifier -&gt; 다른 항목과 구분하기 위해 필요한 정보 즉, URI는 인터넷상의 리소스 자원 자체를 식별하는 고유한 문자열 시퀀스 URL (Uniform Resource Locator) 네트워크상에서 통합 자원(리소스)의 “위치”를 나타내기 위한 규약 웹 사이트 주소 + 컴퓨터 네트워크 상의 자원 특정 웹 페이지의 주소에 접속하기 위해서는 웹 사이트의 주소뿐만 아니라 프로토콜(https, http, sftp, smp 등)을 함께 알아야 접속이 가능한데, URL은 이들 모두를 나타낸다. URN (Uniform Resource Name) 리소스의 위치, 프로토콜, 호스트 등과는 상관없이 각 자원에 이름을 부여한 것 웹 문서의 물리적인 위치와 상관없이 웹 문서 자체를 나타낸다. URI와 URL의 차이점 URI= 식별자, URL=식별자+위치 nan0silver.github.io는 리소스의 이름만 나타내므로 URI https://nan0silver.github.io/는 이름과 위치를 나타내므로 URL (프로토콜 http를 포함하기 때문) URL ⊂ URI URL은 프로토콜과 결합된 상태이다. (프로토콜 + 이름) URI는 그 자체로 이름이 될 수 있다.",
-    "tags": "flutter",
-    "url": "/flutter/2023-08-14-about-UriUrl/"
+    "tags": "backend",
+    "url": "/backend/2023-08-14-about-UriUrl/"
   },{
     "title": "[Flask] Flask서버에 외부 접근하는 방법",
     "text": "app.run()안에 바인딩될 호스트 정보를 넣어준다. app.run()은 127.0.0.1로 실행되며 이는 로컬에서만 실행가능하다. app.run(host=’0.0.0.0’)의 경우 모든 호스트로 접근 가능하다. 포트 변경을 원하는 경우 app.run(host=’0.0.0.0’, port=8000)과 같이 사용한다. 위의 방법으로 실행하면 flask서버를 실행한 컴퓨터의 IP주소와 함께 설정한 포트로 연결가능한 주소가 나온다.",
-    "tags": "flutter",
-    "url": "/flutter/2023-07-01-flask-run/"
+    "tags": "backend",
+    "url": "/backend/2023-07-01-flask-run/"
   },{
-    "title": "[Flutter] Flask 서버와 Flutter 통신",
+    "title": "[Flask] Flask 서버와 통신",
     "text": "목차 Flask란? Dio Future Flask에서 json형태로 response보내는 함수 Flask란? 웹 애플리케이션 개발을 위한 파이썬 프레임워크 가장 유명한 Django(장고)보다 가볍고 필요한 기능만 최대한 라이트한 개발을 할 수 있다. Dio A powerful Http client for Dart http처럼 서버와 통신하기 위해 필요한 패키지 많은 기능을 가지고 있고, 여러가지 커스텀을 쉽게 할 수 있다. pubspec.yaml에 dependency를 추가해주어야 한다. Http와 json형식의 데이터로 받아올 때 차이점 Http로 요청 후 리턴받은 데이터를 decode해준 값이 Dio로 요청 후 리턴받은 데이터와 동일하다. https://kyungsnim.net/175 에서 참고 Future 지금은 없지만 미래에 요청한 데이터 혹은 에러가 담길 그릇 싱글스레드 환경에서 비동기 처리를 위함 비동기 : 어떤 동작이 완료되지 않아도 다음 동작을 수행하는 것 동기 : 모든 동작이 완료된 후 다음 동작을 수행하는 것 Flask에서 json 형태로 response보내는 함수 jsonify json response를 보내기 위해 이미 content-type header가 ‘application/json’로 되어 있는 flask.Response() 객체를 리턴 jsonify도 함수 내부에서도 json form으로 serialize하는 과정에서 json.dumps를 사용 다만 dump하기 전에 받은 값들을 모두 dictionary로 만들었다. Parameter accept dictionary list json.dumps python이 가지고 있는 json library의 json.dumps() 수동으로 MIME type header를 추가해주어야 하는 encoded string을 리턴한다. flask가 알아서 판단해 response를 자동으로 보내주도록 사용하기 때문에 직접적으로 사용할 수 있다. 다만 reponse header fields는 디폴트(text/html; charset=utf-80)로 처리된다. Parameter accept jsonify보다 더 많은 타입",
-    "tags": "flutter",
-    "url": "/flutter/2023-04-18-about-flask/"
+    "tags": "backend",
+    "url": "/backend/2023-04-18-about-flask/"
   },{
     "title": "[Flutter] Flutter UI",
     "text": "Flutter Layout Widget의 생명주기 Flutter Layout Flutter Layout의 핵심은 위젯 위젯은 현재 주어진 상태(데이터)를 기반으로 어떤 UI를 구현할지 정의 플러터 프레임워크는 기존 상태 위젯과 새로운 상태의 위젯을 비교하여 UI변화를 반영 최소한의 리소스로 UI변경을 이룸 레이아웃 모델, 앱 내 이미지, 아이콘, 글자 등 거의 모든 것이 위젯 위젯은 자식을 하나만 갖는 위젯과 자식을 여럿 갖는 위젯으로 나뉨 자식을 하나만 갖는 위젯 container 위젯 자식 위젯을 커스터마이징할 수 있는 위젯 클래스 여백, 간격, 테두리, 배경색 등을 추가할 수 있음 나머지 UI는 속성에 의해 제어됨 (color속성, Text.style속성 등) 자식을 여럿 갖는 위젯 children 매개변수를 입력 받음 리스트로 여러 위젯을 입력할 수 있음 Column 위젯, Row 위젯 children 매개변수에 입력된 모든 위젯들을 세로(가로)로 베치 ListView 위젯 리스트 구현가능, 입력된 위젯이 화면을 벗어나게 되면 스크롤 가능해짐 예시 위의 스크린샷에 해당하는 위젯을 구현하기 위한 위젯 트리이다. ❗️Material apps vs Non-Material apps Material apps 플랫 디자인의 장점을 살리면서도 빛에 따른 종이의 그림자 효과를 이요해 입체감을 살리는 디자인방식을 가진 앱 구글에서 사용하고 있는 플랫 디자인 지침 앱마다 다른 디자인을 통일시키기 위함 Scaffold 위젯 기본적인 material design의 시각적인 레이아웃 구조를 실행한다. Scafflod 위젯 vs Container Scafflod 기본적으로 appbar, body라는 2개의 옵션을 가짐 Container Scafflod의 body부분에 들어가는 부속품 봉지라고 생각하면 됨 한 개의 자식을 가지는 레이아웃 위젯 Container의 생성자는 아래와 같다. Container({ Key key, this.alignment, this.padding, Color color, Decoration decoration, this.foregroundDecoration, double width, double height, BoxConstraints constraints, this.margin, this.transform, this.child, })&lt;/pre&gt; - Widget의 생명주기 Stateful Widget의 생명주기 stateless widget은 한 번 만들어지면 갱신할 수 없어 생명주기가 없다. stateful widget은 10단계의 생명주기가 있다. createState()함수 상태를 생성하는 함수 다른 생명주기 함수들이 포함된 State 클래스를 반환 StatefulWidget 클래스를 상속받는 클래스는 반드시 이 함수를 호출해야 한다. Class MyHomePage extends StatefulWidget { @override _MyHomePageState createState() =&gt; new _MyHomePageState(); } mounted == true createState() 함수가 호출되어 상태가 생성되면 mounted 속성이 true가 된다. 위젯을 제어할 수 있는 buildContext클래스에 접근 가능해짐 buildContext가 활성화되어야 setState()함수 이용 가능 initState()함수 위젯을 초기화하는 함수 주로 데이터 목록을 만들거나 처음 필요한 데이터를 주고받을 때 호출 이 함수를 호출할 때 내부에서 _getJsonData() 함수를 호출하면 서버에서 데이터를 가져와 화면에 출력할 수 있음 didChangeDependencies()함수 initState()함수 호출 후, 해당 위젯이 데이터에 의존하는 위젯이라면 반드시 호출해야하는 함수 주로 상속받은 위젯을 사용할 때 피상속자가 변경되면 호출 build()함수 위젯을 화면에 렌더링하는 함수 (Widget을 반환한다.) @override Widget build(BuildContext context) { return MaterialApp( title: 'Flutter Demo', theme: ThemeData( primarySwatch: Colors.blue, ), home: MyHomePage(title: 'Flutter Demo Page'), ); } didUpdateWidget()함수 부모 위젯이나 데이터가 변경되어 위젯을 갱신해야 할 때 호출하는 함수 initState()함수는 위젯을 초기화할 때 한 번만 호출되므로 위젯이 변경되었을 때 이 함수를 호출해야함 setState()함수 데이터가 변경되었다는 것을 알려주고 이를 반영하여 화면 UI를 변경하는 함수 deactive()함수 위젯의 상태 관리를 중지하는 함수 State객체가 플러터 구성 트리에서 제거될 때 호출됨 dispose()함수를 호출하기 전까지는 State객체를 재사용할 수 있게 해줌 dispose()함수 State객체를 영구적으로 소멸할 때 호출되는 함수 mounted == false 생명주기를 끝내주는 함수 false가 된 다음 이 State는 재사용할 수 없다.",
